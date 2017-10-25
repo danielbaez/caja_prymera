f22c2fbc5e1dc36653fe7f7fdc235aea4cc701c3
Daniel Baez <danielbaez@Mac-mini-de-Comparabien.local>
2017-10-25 18:19:30 -0500

    dswd

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -30,11 +30,11 @@
             <div class="container">
                 <div class="row padding-div-row-header">
                     <div class="col-xs-6 col-title-header-padding">
-                        <h1 class="title-header">&iexcl;Te financiamos hasta el<br>100% de tu auto!*</h1>
+                        <h1 class="title-header">&iexcl;Te financiamos hasta el<br>90% de tu auto!*</h1>
                     </div>
                     <div class="col-xs-6 div-logo">
                         <a href="http://www.prymera.com.pe/" target="_blank"><img alt="" class="img-responsive pull-right img-header" src="<?php echo RUTA_IMG?>fondos/Logo-Prymera-Blanco.png"></a>
-                        <h1 style="display: none">&iexcl;Te financiamos hasta el 100% de tu auto!*</h1>
+                        <h1 style="display: none">&iexcl;Te financiamos hasta el 90% de tu auto!*</h1>
                     </div>
                 </div>    
             </div>            
