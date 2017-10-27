a146e66ba8421c1151179e5ad30f8585f5698fe0
jhonataniberico <jhonatanibericom@gmail.com>
2017-10-27 17:36:29 -0500

    cambios

--- application/controllers/Resumen.php
+++ application/controllers/Resumen.php
@@ -87,7 +87,16 @@ class Resumen extends CI_Controller {
           //cargamos la libreria email de ci
        $this->load->library("email");
        //configuracion para gmail
-       $configGmail = $this->load->config('email', TRUE);
+       $configGmail = array(
+       'protocol' => 'smtp',
+       'smtp_host' => 'ssl://smtp.gmail.com',
+       'smtp_port' => 465,
+       'smtp_user' => 'miauto@prymera.pe',
+       'smtp_pass' => '8hUpuv6da_@v',
+       'mailtype' => 'html',
+       'charset' => 'utf-8',
+       'newline' => "\r\n"
+       );    
        $poliza = null;
        
        //cargamos la configuración para enviar con gmail
@@ -204,7 +213,16 @@ Mayor información y costos (Tasas de interés, comisiones y gastos) están disp
           //cargamos la libreria email de ci
        $this->load->library("email");
        //configuracion para gmail
-       $configGmail = $this->load->config('email', TRUE);   
+       $configGmail = array(
+       'protocol' => 'smtp',
+       'smtp_host' => 'ssl://smtp.gmail.com',
+       'smtp_port' => 465,
+       'smtp_user' => 'miauto@prymera.pe',
+       'smtp_pass' => '8hUpuv6da_@v',
+       'mailtype' => 'html',
+       'charset' => 'utf-8',
+       'newline' => "\r\n"
+       );    
        $poliza = null;
        
        //cargamos la configuración para enviar con gmail
