0945a2b42a09cab09f20e4295e570b1aae3a08dc
jhonataniberico <jhonatanibericom@gmail.com>
2017-10-30 15:08:26 -0500

    cambios

--- application/config/database.php
+++ application/config/database.php
@@ -82,7 +82,6 @@ $db['default'] = array(
 	'database' => 'caja_prymera',*/
 	'username' => 'root',
 	'password' => '',
-	'password' => 'root',
 	'database' => 'caja_prymera',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
