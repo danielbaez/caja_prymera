bed7e4fe37754bc784fcb7dbceff9cbc5c38590a
Daniel Baez <danielbaez@Mac-mini-de-Comparabien.local>
2017-11-13 11:09:03 -0500

    cambios

--- application/controllers/Micash.php
+++ application/controllers/Micash.php
@@ -6,12 +6,8 @@ class Micash extends CI_Controller {
     function __construct() {
         ob_start();
         parent::__construct();
-        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
-        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
-        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
-        $this->output->set_header('Pragma: no-cache');
-        $this->load->model('M_preaprobacion');
 
+        $this->load->model('M_preaprobacion');
         $this->load->helper("url");
         $this->load->helper("access_helper");
         is_logged();
